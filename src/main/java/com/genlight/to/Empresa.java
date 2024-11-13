011f8ed244d5475f3fe97d29f0116bb92387e46b
gustavodscruz <gustavodiasdsc@gmail.com>
2024-11-12 22:19:05 -0300

    add: json binding library and jcbc to pom.xml and start of to's

---
+++ src/main/java/com/genlight/to/Empresa.java
@@ -0,0 +1,50 @@
+package com.genlight.to;
+
+public class Empresa {
+    private int id;
+    private String nome;
+    private String cnpj;
+
+    private String emaill;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmaill() {
+        return emaill;
+    }
+
+    public void setEmaill(String emaill) {
+        this.emaill = emaill;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
